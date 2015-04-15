652f7e09d6db710aec84f40680adaaf72b5a93b4
Matthew Webb <rybka.webb@gmail.com>
2015-04-15 22:24:48 +0100

    First commit of personal landing page site.

---
+++ js/main.js
@@ -0,0 +1,33 @@
+  $(function(){
+
+    $("#typed").typed({
+      strings: ["Welcome to my space on the web!", "Here is where I host my freelance work!", "You can find me on GitHub."],
+      typeSpeed: 30,
+      callback: function(){
+        shift();
+      }
+    });
+
+  });
+
+  function shift(){
+      $(".head-wrap").addClass("shift-text");
+      terminalHeight();
+  }
+
+  function terminalHeight(){
+      var termHeight = $(".terminal-height");
+      var value = termHeight.text();
+      value = parseInt(value);
+      setTimeout(function(){
+          if (value > 10){
+              value = value-1;
+              this.txtValue = value.toString();
+              termHeight.text(this.txtValue);
+              self.terminalHeight();
+          }
+          else{
+              clearTimeout();
+          }
+      }, 10);
+  }
